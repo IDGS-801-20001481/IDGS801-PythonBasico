635642e6a13733850843496e07652187764906d0
IDGS-801-20001481 <gcm1200cmgj@gmail.com>
2023-01-23 19:41:42 -0600

    Actividades 23/01/2023

---
+++ IDGS801-PythonBasico/tema13_my_modulo.py
@@ -0,0 +1,15 @@
+def tem():
+    print("¡Hola desde my_modulo.py!")
+
+
+def tem2():
+    print("adios desde my_modulo.py!")
+
+def main():
+    print("¡Hola desde main!")
+    tem()
+    tem2()
+
+
+if __name__== '__main__':
+    main()
